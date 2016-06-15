97b40b65a9977df28dbcee7391eb5d8b3f93864f
bshock84 <bshock84@yahoo.com>
2016-06-14 19:35:55 -0500

    Main random image selection and presentation mechanism working in a basic mode.

---
+++ Shuffle Pop 2/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  MainView.swift
+//  Shuffle Pop 2
+//
+//  Created by Ben Shockley on 6/13/16.
+//  Copyright © 2016 Ben Shockley. All rights reserved.
+//
+
+import Cocoa
+
+class MainView: NSView {
+
+    let image = NSImageView()
+    
+    
+    
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+        image.image = NSImage(named: "dummy-40x40-Bottles-plain.jpg")
+        self.addSubview(image)
+    }
+    
+}
--- Shuffle Pop 2/ViewController.swift
+++ Shuffle Pop 2/ViewController.swift
@@ -5,23 +5,115 @@
 //  Created by Ben Shockley on 6/13/16.
 //  Copyright © 2016 Ben Shockley. All rights reserved.
 //
-
 import Cocoa
+import Foundation
+import AppKit
+import GameKit
 
 class ViewController: NSViewController {
+    var path: String = "/users/benshockley/Documents/pictures"
+    var randomIndex: Int = 0
+    let mainView = NSView(frame: CGRect(x: 0.0, y: 0.0, width: 600.0, height: 600.0))
+    let imageBox = NSImageView(frame: CGRect(x: 0.0, y: 0.0, width: 600.0, height: 600.0))
+    var previouslyUsedNumbers: [Int] = []
+    var imageArray: [String] = []
+    
+    required init?(coder aDecoder: NSCoder) {
+      super.init(coder: aDecoder)
+    }
+    
+    
+    func configureImageBox() {
+        view.addSubview(mainView)
+        //mainView.translatesAutoresizingMaskIntoConstraints = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        mainView.addSubview(imageBox)
+        imageBox.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activateConstraints([
+        mainView.topAnchor.constraintEqualToAnchor(view.topAnchor),
+        mainView.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor),
+        mainView.leftAnchor.constraintEqualToAnchor(view.leftAnchor),
+        mainView.rightAnchor.constraintEqualToAnchor(view.rightAnchor)])
+        
+        NSLayoutConstraint.activateConstraints([
+        imageBox.topAnchor.constraintEqualToAnchor(mainView.topAnchor),
+        imageBox.leftAnchor.constraintEqualToAnchor(mainView.leftAnchor),
+        imageBox.rightAnchor.constraintEqualToAnchor(mainView.rightAnchor),
+        imageBox.bottomAnchor.constraintEqualToAnchor(mainView.bottomAnchor)])
+        
+        
+        imageBox.imageScaling = NSImageScaling.ScaleProportionallyUpOrDown
+    }
+    
+    func getRandomInt() -> Int {
+        var foundNumber = false
+        var randomIndex: Int = 0
+        
+        while foundNumber == false {
+            randomIndex = GKRandomSource.sharedRandom().nextIntWithUpperBound(imageArray.count)
+            
+            if previouslyUsedNumbers.contains(randomIndex) {
+                if imageArray.count == previouslyUsedNumbers.count {
+                    previouslyUsedNumbers = []
+                    print("\n**** End of array has been reached.  Starting over.****\n")
+                } else {
+                    foundNumber = false
+                }
+            } else {
+                foundNumber = true
+                previouslyUsedNumbers.append(randomIndex)
+            }
+            
+        }
+        return randomIndex
+    }
+    
+    override func viewWillLayout() {
+        configureImageBox()
     }
 
+    func displayRandomImage() {
+        let fileName: String = "\(path)/\(imageArray[randomIndex])"
+        print("The current file is located at \(fileName)\n")
+        imageBox.image = NSImage(contentsOfFile: fileName)
+        randomIndex = getRandomInt()
+
+        
+    }
+    
+    
+    func createImageArray() {
+        let fm = NSFileManager.defaultManager()
+        let items = try! fm.contentsOfDirectoryAtPath(path)
+        
+        for image in items {
+            if image.containsString(".jpg") {
+                imageArray.append(image)
+            }
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createImageArray()
+        configureImageBox()
+        displayRandomImage()
+        NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: #selector(ViewController.displayRandomImage), userInfo: nil, repeats: true)
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    
 }
 
